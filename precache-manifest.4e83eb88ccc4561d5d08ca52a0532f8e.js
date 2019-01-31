6cfc4848aa1f3568d20f5c8e1b0c3312ae317934
Lynn Thompson <lynninla@earthlink.net>
2019-01-30 19:04:11 -0800

    Updates

---
+++ precache-manifest.4e83eb88ccc4561d5d08ca52a0532f8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "94f72689ece81fbc2553",
+    "url": "/ClickyBird/static/js/runtime~main.94f72689.js"
+  },
+  {
+    "revision": "c0f8d8023e591fa31eae",
+    "url": "/ClickyBird/static/js/main.c0f8d802.chunk.js"
+  },
+  {
+    "revision": "7644a9751e730a17c6a5",
+    "url": "/ClickyBird/static/js/1.7644a975.chunk.js"
+  },
+  {
+    "revision": "c0f8d8023e591fa31eae",
+    "url": "/ClickyBird/static/css/main.b0d1dc45.chunk.css"
+  },
+  {
+    "revision": "0b1dc58d03540184d7af044213b47d3c",
+    "url": "/ClickyBird/index.html"
+  }
+];
